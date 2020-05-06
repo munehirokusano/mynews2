0c4d40352dbf25ee25a8a564d3ba01df9f399bd7
名前 <>
2020-05-06 02:12:47 +0000

    課題提出09

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
+
+//課題3
+
+//Route::get('XXXXXX','XXX\AAAcountloller@BBB');
+
+
+/*課題4
+
+コード、20番目、21番目に追記しました。
+
+*/
