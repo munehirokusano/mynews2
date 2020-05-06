ee255f30ea0c40f5224ea5d7384831ad0159ef73
名前 <>
2020-05-06 04:09:33 +0000

    課題提出10

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+    
+    <head>
+        
+        <meta charset="utf-8">
+        
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <titile>MyProfile</titile>
+        
+    </head>
+    
+    <body>
+        
+        <h1>Myプロフィール作成画面</h1>
+        
+    </body>
+    
+</html>
