ea0383d6bae400a2d54bc0c5b036da7a7ca2d8da
名前 <>
2020-05-08 21:44:13 +0000

    課題提出13

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -18,6 +18,67 @@
                 
                 <h2>ニュース新規作成</h2>
                 
+                <from action="{{ action('Admin\NewsController@create') }}" method="post"
+enctype="multipart\from-data">
+                    
+                    @if (count($errors) > 0)
+                    
+                        <ul>
+                            
+                            @foreach($errors->all() as $e)
+                            
+                                <li>{{ $e }}</li>
+                            
+                            @endforeach
+                            
+                        </ul>
+                    
+                    @endif
+                    
+                    <div class="form-group row">
+                        
+                        <label class="col-md-2">タイトル</label>
+                        
+                        <div class="col-md-10">
+                            
+                            <input type="text" class="form-control" name="title" value="{{
+old('title') }}">
+                            
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="form-group row">
+                        
+                        <label class="col-md-2">本文</label>
+                        
+                        <div class="col-md-10">
+                            
+                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}
+</textarea>
+                            
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="form-group row">
+                        
+                        <label class="col-md2">画像</label>
+                        
+                        <div class="col-md-10">
+                            
+                            <input type="file" class="form-control-file" name="image">
+                            
+                        </div>
+                        
+                    </div>    
+                        
+                    {{ csrf_field() }}
+                        
+                    <input type="submit" class="bth bth-primary" value="更新">
+                        
+                </from>
+                
             </div>
             
         </div>
