be5bc4048b11ce00771c325d069f66365cff5ad5
名前 <>
2020-05-11 10:59:54 +0000

    追加

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    //以下を追記(課題5)
+    
+    public function add()
+    {
+        return view('admin.profile.create');
+        
+    }
+    
+    public function create(Request $request)
+    {
+        return redirect('admin/profile/create');
+    }
+    
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+    
+    public function update(Request $request)
+    {
+        return redirect('admin/profile/edit');
+    }
+    
+}
