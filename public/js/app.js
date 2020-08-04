195bc890dad88f286b7d347e59686d24ebce7eb8
m-kusano <mk09078193819@gmail.com>
2020-08-04 21:56:34 +0900

    ヘロクデプロイ

--- public/js/app.js
+++ public/js/app.js
@@ -49784,11 +49784,11 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/ec2-user/environment/mynews/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /home/ec2-user/environment/mynews/resources/sass/app.scss */"./resources/sass/app.scss");
-__webpack_require__(/*! /home/ec2-user/environment/mynews/resources/sass/admin.scss */"./resources/sass/admin.scss");
-__webpack_require__(/*! /home/ec2-user/environment/mynews/resources/sass/profile.scss */"./resources/sass/profile.scss");
-module.exports = __webpack_require__(/*! /home/ec2-user/environment/mynews/resources/sass/front.scss */"./resources/sass/front.scss");
+__webpack_require__(/*! C:\work\mynews2\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\work\mynews2\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\work\mynews2\resources\sass\admin.scss */"./resources/sass/admin.scss");
+__webpack_require__(/*! C:\work\mynews2\resources\sass\profile.scss */"./resources/sass/profile.scss");
+module.exports = __webpack_require__(/*! C:\work\mynews2\resources\sass\front.scss */"./resources/sass/front.scss");
 
 
 /***/ })
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,13 +9,13 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title')</title>
         <!-- scripts -->
-        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
         <!-- Fonts -->
         <link rel="dns-prefetch" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
         <!-- styles -->
-        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
